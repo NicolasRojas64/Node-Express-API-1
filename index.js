b95616989a2339351cd9664b5729c83a950ba338
NicolasRojas64 <nicolas.rojas04@uptc.edu.co>
2023-09-03 16:12:15 -0500

    Rutas de métodos POST, GET, PUT, DELETE

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hola");
+
+app.use(express.json())
+
+//Middleware, manejador de peticiones
+function logger(req,res,next){
+    
+}
+
+app.all("/user", (req,res,next)=>{
+    console.log("Usarios");
+    next();
 })
 
-app.listen(3000, () => {
-    console.log("Server up on port 3000!!!");
+app.get("/", (req, res) => {
+    res.send("<h1>Puerto 5000<h1/>");
 })
+
+app.get("/user", (req,res) =>{
+    res.json({
+        "name":"nick",
+        "lastName":"Rojas"
+    })
+})
+
+app.post("/user/:id", (req,res)=>{
+    console.log(req.body);
+    console.log(req.params);
+    res.send(`Usuario ${req.params.id} agregado`);
+})
+
+app.delete( '/delete', (req, res) => {
+    res.send("DELETE METHOD")
+})
+
+app.listen(5000, () => {
+    console.log("Server up on port 5000!!!");
+})
+
